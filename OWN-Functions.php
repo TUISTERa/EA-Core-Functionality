a4984027a1ce6beb73cbdbe17cd4f7858b685117
TUISTERa <tuistera@gmail.com>
2019-11-28 17:15:17 +0200

    Update and rename core-functionality.php to OWN-Functions.php
    First edition of my plugin

--- OWN-Functions.php
+++ OWN-Functions.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name: Core Functionality
+ * Plugin Name: OWN Functions
  * Description: This contains all your site's core functionality so that it is theme independent. <strong>It should always be activated</strong>.
- * Version:     1.2.0
- * Author:      Bill Erickson & Jared Atchison
+ * Version:     1.0.0
+ * Author:      TUISTERa | 
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2, as published by the
@@ -13,16 +13,17 @@
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.
- *
- * @package    CoreFunctionality
+ * Original source from Bill Erickson & Jared Atchison
+ * 
+@package    OWNFunctions
  * @since      1.0.0
- * @copyright  Copyright (c) 2014, Bill Erickson & Jared Atchison
+ * @copyright  Copyright (c) 2019, Georgi Popov
  * @license    GPL-2.0+
  */
 
 // Plugin directory
 define( 'EA_DIR' , plugin_dir_path( __FILE__ ) );
 
-require_once( EA_DIR . '/inc/general.php' );
-require_once( EA_DIR . '/inc/wordpress-cleanup.php' );
-require_once( EA_DIR . '/inc/kill-trackbacks.php' );
+//require_once( EA_DIR . '/inc/general.php' );
+//require_once( EA_DIR . '/inc/wordpress-cleanup.php' );
+//require_once( EA_DIR . '/inc/kill-trackbacks.php' );
