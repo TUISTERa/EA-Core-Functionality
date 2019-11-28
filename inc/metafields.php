5427610a88129b60eda2ea3c370698fed734b5cc
TUISTERa <tuistera@gmail.com>
2019-11-28 18:04:27 +0200

    Create metafields.php

---
+++ inc/metafields.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Disable Custom Fields metabox
+ *
+ * @package      OWNFunctions
+ * @author       TUISTERa
+ * @since        1.0.0
+ * @license      GPL-2.0+
+**/
+/*
+Plugin Name: Disable Custom Fields metabox
+Plugin URI: http://gvp.icu
+Description: Remove Ancient Custom Fields metabox from post editor
+because it uses a very slow query meta_key sort query 
+so on sites with large postmeta tables it is super slow
+and is rarely useful anymore on any site
+Version: 1.0
+Author: Georgi Popov
+Author URI: http://gvp.icu
+*/
